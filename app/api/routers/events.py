0f60cdceeffc99b7c7faf86f278ae0679f78fe9b
mandmsyara <115021167+mandmsyara@users.noreply.github.com>
2026-04-20 01:14:57 +0300

    sync | events add

--- app/api/routers/events.py
+++ app/api/routers/events.py
@@ -1,10 +1,12 @@
-from fastapi import APIRouter, Depends, status
+from fastapi import APIRouter, Depends, HTTPException, status
+from uuid import UUID
 from sqlalchemy.ext.asyncio import AsyncSession
 
 from app.database.session import get_async_session
 from app.services.events_api import EventsProviderClient
 from app.repositories.events import EventRepository
 from app.services.sync_service import EventSyncService
+from app.schemas.event_schema import EventRead
 
 from app.core.config import EXTERNAL_API_URL
 
@@ -21,3 +23,22 @@ async def sync_events(session: AsyncSession = Depends(get_async_session)):
     await service.sync_all()
 
     return {"status": "ok"}
+
+
+@router.get("/", response_model=list[EventRead])
+async def get_events(
+    limit: int = 10, offset: int = 0, session: AsyncSession = Depends(get_async_session)
+):
+    repo = EventRepository(session)
+    events = await repo.get_all_events(limit=limit, offset=offset)
+    return events
+
+
+@router.get("/{event_id}/", response_model=EventRead)
+async def get_event(event_id: UUID, session: AsyncSession = Depends(get_async_session)):
+    repo = EventRepository(session)
+    event = await repo.get_events_by_id(event_id)
+
+    if not event:
+        raise HTTPException(status_code=404, detail="Event not found")
+    return event
--- app/repositories/events.py
+++ app/repositories/events.py
@@ -1,7 +1,10 @@
+from sqlalchemy import select
+from sqlalchemy.orm import joinedload
 from sqlalchemy.ext.asyncio import AsyncSession
 from sqlalchemy.dialects.postgresql import insert
 from app.models.event import Event
 from app.models.place import Place
+import uuid
 
 
 class EventRepository:
@@ -21,3 +24,19 @@ class EventRepository:
         stmt = stmt.on_conflict_do_update(index_elements=["id"], set_=event_data)
 
         await self.session.execute(stmt)
+
+    async def get_all_events(self, limit: int = 100, offset: int = 0):
+        stmt = (
+            select(Event).options(joinedload(Event.place)).limit(limit).offset(offset)
+        )
+
+        result = await self.session.execute(stmt)
+        return result.scalars().all()
+
+    async def get_events_by_id(self, event_id: uuid.UUID):
+        stmt = (
+            select(Event).where(Event.id == event_id).options(joinedload(Event.place))
+        )
+        result = await self.session.execute(stmt)
+
+        return result.scalar_one_or_none()
