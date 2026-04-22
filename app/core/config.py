2707ca776d5fcbeeab1e2ec46dbca63cc1841a8c
mandmsyara <115021167+mandmsyara@users.noreply.github.com>
2026-04-22 11:59:09 +0300

    fix migrations

--- app/core/config.py
+++ app/core/config.py
@@ -12,10 +12,6 @@ POSTGRES_DB = os.getenv("POSTGRES_DATABASE_NAME")
 
 DATABASE_URL = f"postgresql+asyncpg://{POSTGRES_USER}:{POSTGRES_PASSWORD}@{POSTGRES_HOST}:{POSTGRES_PORT}/{POSTGRES_DB}"
 
-# DATABASE_URL = os.getenv(
-#     "DATABASE_URL", "postgresql+asyncpg://postgres:postgres@localhost:5432/events_db"
-# )
-
 EXTERNAL_API_KEY = os.getenv("X_API_KEY")
 EXTERNAL_API_URL = (
     "http://student-system-events-provider-web.student-system-events-provider.svc:8000"
